f5375da1f0ed5f52ca6f1f03898d73545ac6e63a
satheesh90 <satheesh.retros@gmail.com>
2016-06-21 13:42:57 +0200

    create

---
+++ ledtest.py
@@ -0,0 +1,41 @@
+import time
+import sys
+
+from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+		
+		
+
+if __name__ == '__main__':
+	# Create NeoPixel object with appropriate configuration.
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+	strip.begin()
+
+	delay = sys.argv[1]
+	
+	print ('Press Ctrl-C to quit.')
+	while True:
+	  # http://stackoverflow.com/questions/1009860/command-line-arguments-in-python
+	  # Color wipe animations.
+	  colorWipe(strip, Color(255, 255, 255))  
+	  time.sleep(delay)
+	  colorWipe(strip, Color(0, 0, 0))  
+	  time.sleep(delay)
